d14984a96bc769c13072e6c1d63ba0eafc0a0f72
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-08 15:29:07 +0700

    prime number done

---
+++ primeNum.py
@@ -0,0 +1,13 @@
+firstNum = 1
+secondNum = 100
+
+
+for betNum in range (firstNum, secondNum+1):
+    if betNum > 1:
+        for primeNum in range (2, betNum):
+            if betNum % primeNum == 0:
+                break
+        else:
+            print(betNum)
+        
+        
