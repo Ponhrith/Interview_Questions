5ad825809bf025bb71ace9c8045137c8772ce6fa
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-11 16:24:41 +0700

    testing

--- primeNum.py
+++ primeNum.py
@@ -1,5 +1,6 @@
 firstNum = 1
 secondNum = 100
+amount = 0
 
 for betNum in range (firstNum, secondNum+1):
     if betNum > 1:
@@ -8,6 +9,13 @@ for betNum in range (firstNum, secondNum+1):
                 break
         else:
             print(betNum)
+            amount = amount + 1
+print("The amount of prime number is:", amount)
 
 
-       
+# amount = 0
+#  for num in range (100, 200):
+#      if all(num%i !=0 for i in range (2,num)):
+#          print(num)
+        #  amount = amount + 1
+# print(amount)
