684da02c0b574e477252ba174cf2591536b9825e
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-13 21:45:34 +0700

    testing

---
+++ 9_search.py
@@ -0,0 +1,9 @@
+array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+type = int(input("Type a number: "))
+
+for i in range(array):
+    if i == type:
+        print("True")
+    else:
+        print("False")
+            
--- sort_non_func.py
+++ sort_non_func.py
@@ -1,16 +1,33 @@
-list = [100,12,10,-10,0.5,0.1,12.3]
+# list = [100,12,10,-10,0.5,0.1,12.3] #computer put in a memory
+# sortList = []
+# #i = 0
+# while list:
+#     firstIndex = list[0]
+#     for i in list: # i = 12 respectively
+#         #firstIndex = list[0]
+#         # secondIndex = list.index(100.0, firstIndex + 1)
+#         if firstIndex > i:
+#             firstIndex = i # to find the new minimum, first 12 is the smallest, then it swaps to the smaller
+#     sortList.append(firstIndex)
+#     list.remove(firstIndex)
+        
+# print(sortList)
+
+#-------------------------------------------------------------
+
+#reverse
+    
+list = [100,12,10,-10,0.5,0.1,12.3] #computer put in a memory
 sortList = []
 #i = 0
 while list:
     firstIndex = list[0]
-    for i in list:
+    for i in list: # i = 12 respectively
         #firstIndex = list[0]
         # secondIndex = list.index(100.0, firstIndex + 1)
-        if firstIndex > i:
-            firstIndex = i
+        if firstIndex < i:
+            firstIndex = i # to find the new minimum, first 12 is the smallest, then it swaps to the smaller
     sortList.append(firstIndex)
     list.remove(firstIndex)
         
-print(sortList)
-    
-        
+print(sortList)       
