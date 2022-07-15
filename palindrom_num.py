2f491a57b1b1819eff9fba065369e0325a4333af
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-15 15:11:22 +0700

    working

---
+++ palindrom_num.py
@@ -0,0 +1,11 @@
+num = 1221
+temp = num
+reverse_num = 0
+while num!=0:
+    remainder = num % 10
+    reverse_num = reverse_num * 10 + remainder 
+    num = num // 10 
+if temp== reverse_num:
+    print('True')
+else:
+    print('False')
