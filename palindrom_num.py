268f8899a784960a9cfe6a844cf82d61c796a440
Ponhrith <ponhrith.nhep@gmail.com>
2022-07-15 15:44:06 +0700

    done

--- palindrom_num.py
+++ palindrom_num.py
@@ -4,8 +4,9 @@ reverse_num = 0
 while num!=0:
     remainder = num % 10
     reverse_num = reverse_num * 10 + remainder 
-    num = num // 10 
-if temp== reverse_num:
+    num = num // 10
+     
+if temp == reverse_num:
     print('True')
 else:
     print('False')
